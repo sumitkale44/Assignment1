17d2687c6f32105ee399caf716d3f799c3182847
Sumit Kale <68341145+sumitkale44@users.noreply.github.com>
2021-10-07 20:30:56 +0530

    Add files via upload

--- Assignment1/cose.js
+++ Assignment1/cose.js
@@ -20,6 +20,9 @@ const initactivity = function () {
   const answers = document.createElement("div");
   const content = document.createElement("div");
   const blankDiv=document.createElement('div');
+  const reset=document.createElement('div');
+  const showMe=document.createElement('div');
+  const submit=document.createElement('div');
 
   title.className = "titleClass";
   subTitle.className = "subTitleClass";
@@ -30,6 +33,14 @@ const initactivity = function () {
   blankDiv.className="blankDivClass";
   answers.className = "answersClass";
   content.className = "contentClass";
+  reset.className="resetClass    resetDisableClass  ";
+  showMe.className="showMeClass";
+  submit.className="submitClass";
+
+
+
+
+  reset.classList.remove('resetDisableClass');
 
 
   title.innerHTML = responseData.courseTitle;
@@ -37,6 +48,11 @@ const initactivity = function () {
   pageTitle.innerHTML = responseData.pageTitle;
   instruction.innerHTML = responseData.instruction;
   instruction1.innerHTML = responseData.instruction1;
+  // reset = responseData.resetButton.image;
+  // reset.innerHTML=responseData.resetButton.image;
+
+  
+  console.log(reset);
 
   container.append(title);
   container.append(subTitle);
@@ -47,6 +63,10 @@ const initactivity = function () {
   content.append(questions);
   content.append(blankDiv);
   content.append(answers);
+  container.append(reset);
+  container.append(showMe);
+  container.append(submit);
+  
   
  
 
@@ -69,26 +89,50 @@ const initactivity = function () {
     const line_Break = document.createElement("br");
     const ansDiv = document.createElement("div");
     ansDiv.className = "ansDivClass";
-    ansDiv.append(lineBreak);
+    ansDiv.append(line_Break);
     ansDiv.append(answerText);
-    console.log(answerText);
     answers.append(ansDiv);
     
 
+
     
   }
-  // for (let i = 0; i < responseData.options.length; i++){
-  //   const blankDiv= document.createElement('div');
-  //   blankDiv.className="blankDivClass";
-  //   blankDiv.append(content);
-  //   console.log(blankDiv);
+  // ansDiv.addEventListener("mouseDown",onMouseDown);
+  // function onMouseDown(e){
+  //   pointer = e.target;
+  //   ansDiv.addEventListener("mousemove", onMouseMove);
+  //   bDiv.addEventListener("mouseup", onMouseUp);
   // }
+  // function onMouseMove(e){
+  //   pointer.left = e.pageX;
+  //   pointer.top = e.pageY;
+  // }
+  // function onMouseUp(e){
+  //   var bool = false;
+  //   for(var i=0; i<obj.options.length; i++){
+  //     if( bool = true){
+       
+  //       pointer.left = this["rect_"+i].left;
+  //       pointer.top = this["rect_"+i].right;
+  //       break;
+  //     }
+  //   }
+  //   if(!bool){
+  //     pointer.left = org.x;
+  //     pointer.top = org.y;
+  //   }
+  //   document.removeEventListener("mousemove", onMouseMove);
+  //   document.removeEventListener("mouseup", onMouseUp);
+  // }
+  // function onMouseMove(e){
+  //   pointer.left = e.pageX;
+  //   pointer.top = e.pageY;
+  // }
+
+  // reset.addEventListener('click',function(){
+  //   reset.classList.add('resetDisableClass');
+  // })
+   
 };
-
-//
-//
-
-// const text = document.createTextNode(responseData.options[i]['question']);
-//      options.innerHTML=responseData.options[i]['question'];
 
 
